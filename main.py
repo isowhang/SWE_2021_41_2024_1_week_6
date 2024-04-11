57ab802626d49bc8e47d83b58e04f2b9dc264ed7
isowhang <isowhang@g.ssku.edu>
2024-04-11 09:52:23 +0000

    init: main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]
+        sizeOfNums = len(nums)
+        result = ""
+        for i in range(0, sizeOfNums, 1):
+            for j in range(i, sizeOfNums, 1):
+                if(nums[i]+nums[j] == target):
+                  result = "[" + i +"," + j + "]"
+        return result
+
+    
+    
