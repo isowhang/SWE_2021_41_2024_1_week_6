217fe22a90773854af6ffb9c7a05fccee5ad94c1
isowhang <isowhang@g.ssku.edu>
2024-04-11 10:09:36 +0000

    4th: main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]
         sizeOfNums = len(nums)
-        result = ""
+        result = []
         for i in range(0, sizeOfNums, 1):
             for j in range(i, sizeOfNums, 1):
                 if(nums[i]+nums[j] == target):
-                  result = "[" + i +"," + j + "]"
-        
+                  result = [i,j]
+
         return result   
